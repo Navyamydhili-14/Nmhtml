5a4b6f23f327defb2aaad236c3ddfc294f10d791
Navyamydhili-14 <navyamydhilikorlu@gmail.com>
2026-06-09 09:12:28 +0530

    Committed

--- Moviecharge.java
+++ Moviecharge.java
@@ -1,6 +1,6 @@
 class Moviecharge {
     public static void main(String[] args) {
-        int age=65;
+        int age = 65;
         if (age < 13) {
             System.out.println("The ticket price is 80");
         } else if (age >= 13 && age <= 59) {
